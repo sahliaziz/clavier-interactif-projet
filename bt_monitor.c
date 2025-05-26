ab498128516d2afaf29c71f574118f46421312a3
sahliaziz <mohamed-aziz.sahli@univ-tlse3.fr>
2025-05-26 07:39:00 +0200

    migrate to gpiod

--- bt_monitor.c
+++ bt_monitor.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <wiringPi.h>
+#include <gpiod.h>
+#include <time.h>
 
-// Constants
-#define LED_PIN 6          // physical pin 22 (wiringPi pin 6)
-#define BUTTON_PIN 10       // physical pin 24 (wiringPi pin 10)
-#define DEVICE_MAC "XX:XX:XX:XX:XX:XX"
-#define CHECK_INTERVAL 10   // seconds
+#define CHIP_NAME "gpiochip0"
+#define BUTTON_LINE 25    // BCM GPIO 25
+#define LED_LINE 8       // BCM GPIO 08
 
-// Function to attempt Bluetooth connection
-int connect_bluetooth(const char *mac) {
+#define BT_MAC "DA:FE:25:0E:EE:19"
+#define CHECK_INTERVAL 20  // seconds
+
+// Connect to the Bluetooth speaker using bluetoothctl
+void connect_bluetooth(const char *mac) {
     char command[128];
     snprintf(command, sizeof(command), "echo -e 'connect %s\nexit' | bluetoothctl > /dev/null", mac);
-    return system(command);
+    system(command);
 }
 
-// Function to check if Bluetooth device is connected
+// Check if the Bluetooth speaker is connected
 int is_connected(const char *mac) {
     char command[128];
     snprintf(command, sizeof(command), "bluetoothctl info %s | grep 'Connected: yes' > /dev/null", mac);
@@ -25,32 +27,63 @@ int is_connected(const char *mac) {
 }
 
 int main() {
-    wiringPiSetup();
+    struct gpiod_chip *chip;
+    struct gpiod_line *button, *led;
+    int ret;
 
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(BUTTON_PIN, INPUT);
-    pullUpDnControl(BUTTON_PIN, PUD_UP); // Button active LOW
+    chip = gpiod_chip_open_by_name(CHIP_NAME);
+    if (!chip) {
+        perror("Open chip failed");
+        return 1;
+    }
+
+    button = gpiod_chip_get_line(chip, BUTTON_LINE);
+    led = gpiod_chip_get_line(chip, LED_LINE);
+
+    if (!button || !led) {
+        perror("Get line failed");
+        return 1;
+    }
+
+    // Request input (button) with bias pull-up
+    ret = gpiod_line_request_input_flags(button, "bt_button", GPIOD_LINE_REQUEST_FLAG_BIAS_PULL_UP);
+    if (ret < 0) {
+        perror("Request button input failed");
+        return 1;
+    }
+
+    // Request output (LED)
+    ret = gpiod_line_request_output(led, "bt_led", 0);
+    if (ret < 0) {
+        perror("Request LED output failed");
+        return 1;
+    }
 
     printf("Attempting initial connection...\n");
-    connect_bluetooth(DEVICE_MAC);
+    connect_bluetooth(BT_MAC);
 
     while (1) {
-        // Check button press
-        if (digitalRead(BUTTON_PIN) == LOW) {
-            printf("Button pressed. Reconnecting...\n");
-            connect_bluetooth(DEVICE_MAC);
-            delay(1000);
+        int value = gpiod_line_get_value(button);
+        if (value == 0) {  // button pressed (active low)
+            printf("Button pressed: reconnecting...\n");
+            if (is_connected(BT_MAC)) {
+                printf("Already connected.\n");
+            } else {
+                printf("Connecting to Bluetooth speaker...\n");
+                connect_bluetooth(BT_MAC);
+            sleep(1); // debounce delay
+            }
         }
 
-        // Check connection status
-        if (is_connected(DEVICE_MAC)) {
-            digitalWrite(LED_PIN, HIGH);
+        if (is_connected(BT_MAC)) {
+            gpiod_line_set_value(led, 1);
         } else {
-            digitalWrite(LED_PIN, LOW);
+            gpiod_line_set_value(led, 0);
         }
 
         sleep(CHECK_INTERVAL);
     }
 
+    gpiod_chip_close(chip);
     return 0;
 }
